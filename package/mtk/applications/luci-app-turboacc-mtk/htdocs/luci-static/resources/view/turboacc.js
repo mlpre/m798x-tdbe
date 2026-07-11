afd55d8d1a7c54604b957ed905b3386ea0372d34
sukerxi <sukerin@qq.com>
2026-07-11 22:44:23 +0800

    Revert "fix(turboacc): 调整 fastpath_mh_eth_hnat 默认值为禁用"
    This reverts commit 9a56e74c73314104785f51ba622f22bd01910889.

--- package/mtk/applications/luci-app-turboacc-mtk/htdocs/luci-static/resources/view/turboacc.js
+++ package/mtk/applications/luci-app-turboacc-mtk/htdocs/luci-static/resources/view/turboacc.js
@@ -209,7 +209,7 @@ return view.extend({
 
 		o = s.option(form.Flag, 'fastpath_mh_eth_hnat', _('Enable ethernet HNAT'),
 			_('Enable hardware offloading for wired connections.'));
-		o.default = o.disabled;
+		o.default = o.enabled;
 		o.rmempty = false;
 		o.depends('fastpath', 'mediatek_hnat');
 
